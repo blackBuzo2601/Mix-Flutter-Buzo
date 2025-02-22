4bb21665d6a18fa3e87093b8123106dcb8cd411b
blackBuzo2601 <elianpro58@gmail.com>
2025-02-21 17:53:06 -0800

    Creada clase para manejar switch que construye la pantalla principal. Falta envolver y consumir Provider

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,20 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:provider_mix/screens/pantalla_dos.dart';
+import 'package:provider_mix/screens/pantalla_tres.dart';
 import 'package:provider_mix/screens/pantalla_uno.dart';
 import 'package:provider_mix/widgets/custom_bottom_nav_bar.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: Icon(Icons.movie_filter_outlined),
-        title: Text("Menú Principal "),
-        centerTitle: true,
-      ),
-      body: PantallaUno(),
-      bottomNavigationBar: CustomBottomNavBar(),
-    );
+        appBar: AppBar(
+          leading: Icon(Icons.movie_filter_outlined),
+          title: Text("Menú Principal "),
+          centerTitle: true,
+        ),
+        body: _IndiceSeleccionado(),
+        bottomNavigationBar: CustomBottomNavBar());
+  }
+}
+
+class _IndiceSeleccionado extends StatelessWidget {
+  const _IndiceSeleccionado({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    int indice = 0;
+
+    switch (indice) {
+      case 0:
+        return PantallaUno();
+      case 1:
+        return PantallaDos();
+      case 2:
+        return PantallaTres();
+      default:
+        return PantallaUno();
+    }
   }
 }
